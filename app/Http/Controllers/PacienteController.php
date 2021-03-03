f20e65253f6aa6486d8978d88abe79fc60b7edac
nreal <nreal@makignsense.com>
2021-03-02 23:47:12 -0300

    Add create paciente

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Paciente;
+use App\Http\Requests\PacienteCreateRequest;
 use DB;
 
 class PacienteController extends Controller
@@ -18,5 +19,29 @@ class PacienteController extends Controller
     public function create()
     {
         return view('paciente.create');
+    }
+
+    public function store(PacienteCreateRequest $request)
+    {
+        $paciente = new Paciente;
+        $paciente->nombre = $request->get('nombre');
+        $paciente->apellido = $request->get('apellido');
+        $paciente->dni = $request->get('dni');
+        $paciente->email = $request->get('email');
+        $paciente->nroHistorial = $request->get('nroHistorial');
+        $paciente->address = $request->get('address');
+        $paciente->city = $request->get('city');
+        $paciente->postalCode = $request->get('postalCode');
+        $paciente->country = $request->get('country');
+        $paciente->nationality = $request->get('nationality');
+        $paciente->socialWork = $request->get('socialWork');
+        $paciente->nroAfiliado = $request->get('nroAfiliado');
+        $paciente->state = $request->get('state');
+        $paciente->genero = $request->get('genero');
+        $paciente->pronombre = $request->get('pronombre');
+        $paciente->nivelEducativo = $request->get('nivelEducativo');
+        $paciente->isActive = $request->get('isActive');
+        $paciente->save();
+        return redirect('/paciente/create')->with('message','Guardado Satisfactoriamente !');
     }
 }
--- resources/views/paciente/create.blade.php
+++ resources/views/paciente/create.blade.php
@@ -8,11 +8,33 @@
     <div class="container">
         <div class="col-sm-8">
             <h1>Formulario en laravel</h1>
-            <form ation="/guardar" method="POST">
+            <form action="{{url('paciente')}}" method="POST">
+            @csrf
                 <div class="form-group row">
                     <label for="nombre" class="col-sm-2 col-form-label">Nombre</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="nombre" placeholder="Nombre">
+                        <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="apellido" class="col-sm-2 col-form-label">Apellido</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="apellido" name="apellido" placeholder="Apellido">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="dni" class="col-sm-2 col-form-label">Dni</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="dni" name="dni" placeholder="Dni">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="email" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="email" name="email" placeholder="email">
                     </div>
                 </div>
                 
