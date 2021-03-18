3fcc36512e82f113d3d0ed967c9711d6dd1a8480
nreal <nreal@makignsense.com>
2021-03-18 20:43:44 -0300

    Add auth PacienteController

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -9,6 +9,11 @@ use DB;
 
 class PacienteController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         $pacientes['pacientes'] = Paciente::paginate(3);
