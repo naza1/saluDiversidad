0a8094643afe48aa37acc831bbc8c54424438b34
nreal <nreal@makignsense.com>
2021-03-18 20:10:33 -0300

    Add login and register

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 
 Route::resource('/paciente', 'App\Http\Controllers\PacienteController');
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
