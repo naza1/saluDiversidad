6a73498b9af449cf8767d678ca2b3f7f26c6ed71
gvfernan <gvfernan07@gmail.com>
2021-04-28 14:57:52 -0300

    PDF para estudios

--- resources/views/pdf/estudios.blade.php
+++ resources/views/pdf/estudios.blade.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hi</title>
+    <title>Orden estudios</title>
+    
 </head>
 <body>
-    <h1>Nombre {{ $nombre }}</h1>
-    <p>Apellido {{ $apellido }}</p>
-    <p>Dni {{ $dni }}</p>
-    <p>Fecha {{ $fecha }}</p>
-    @foreach($estudios as $estudio)
-    <p>{{ $estudio }}</p>
-    @endforeach
+<div id="content" class="col" style="width: 400px; margin: auto; min-height: 700px !important; border-color: gray; border-width: 1px; border-style: solid; padding: 40px 20px 20px 20px;">
+<div>
+    <div style="width: 200px; display: inline-block;"><p style="font-family: Verdana,Geneva,sans-serif; font-weight: 700; font-size: 20px;">Unidades sanitarias</p></div>
+    <div style="width: 150px; display: inline-block;"><img src="{{ asset('img/logo_mgp_sin_fondo.png') }}"></div>
+    <hr style="height: 10px; background-color: black; width: 100%;">
+</div>
+    <p style="font-family: Verdana,Geneva,sans-serif;">Nombre y Apellido: {{ $nombre }} {{ $apellido }}</p>
+    <p style="font-family: Verdana,Geneva,sans-serif;">DNI: {{ $dni }}</p>
+    <hr style="height: 10px; background-color: #cccccc; border-color: #cccccc; width: 100%;">
+    <p style="font-family: Verdana,Geneva,sans-serif;">Rp/</p>
+    <div style="background-image: url('{{ asset('img/back_receta.png')}}');background-repeat: no-repeat; background-position: center; min-height: 75% !important;">
+            @foreach($estudios as $estudio)
+        <p style="font-family: Verdana,Geneva,sans-serif; text-align: center; font-size: 10px;">{{ $estudio }}</p>
+        @endforeach
+    
+    <p style="font-family: Verdana,Geneva,sans-serif; text-align: right;"><img src="{{ asset('img/firma.png') }}"></p>
+    <p style="font-family: Verdana,Geneva,sans-serif; text-align: right;">Mar del Plata {{ $fecha }}</p>
+</div>
+    <hr style="height: 10px; background-color: black; width: 100%; ">
+</div>
 </body>
 </html>
