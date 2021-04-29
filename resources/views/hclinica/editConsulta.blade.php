bccda50e6f8f0705d22caa6c2ed007a6fb25ea04
gvfernan <gvfernan07@gmail.com>
2021-04-29 15:55:56 -0300

    segunda vista historia clinica

---
+++ resources/views/hclinica/editConsulta.blade.php
@@ -0,0 +1,23 @@
+@include('layouts.app')
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Opciones para paciente</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="{{ asset('js/laravel.js') }}"></script>
+    </head>
+<body>
+<nav aria-label="breadcrumb" style="padding-top: -50px !important;">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="{{ url('/dashboardAdmin') }}">Inicio</a></li>
+    
+  </ol>
+</nav>
+
+
+     
+
+@include('layouts.footer')
+</body>
+</html>
