49f69ebf799e69dfb8de4db0c6f0981512724465
gvfernan <gvfernan07@gmail.com>
2021-04-28 23:34:37 -0300

    Modificación de plantilla de email

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,12 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td class="content-cell" align="center">
+    <p>Programa Municipal en Salud y Diversidad - MGP Salud - Copyright 2021</p>
+<!--{{ Illuminate\Mail\Markdown::parse($slot) }}-->
+</td>
+</tr>
+</table>
+</td>
+</tr>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="http://c2170987.ferozo.com/wp-content/uploads/2021/03/logo.png" class="logo" alt="Salud y diversidad - MGP">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
