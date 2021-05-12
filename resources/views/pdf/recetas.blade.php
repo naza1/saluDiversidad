c3869fb13af775f1a348a258b7cf5b6a61c17090
gvfernan <gvfernan07@gmail.com>
2021-05-12 12:15:48 -0300

    arreglo de desastre naza receta

--- resources/views/pdf/recetas.blade.php
+++ resources/views/pdf/recetas.blade.php
@@ -5,7 +5,9 @@
     
 </head>
 <body>
-<<div>
+<div id="content" class="col" style="width: 400px; margin: auto; min-height: 700px !important; border-color: gray; border-width: 1px; border-style: solid; padding: 40px 20px 20px 20px;">
+<div>
+
     <div style="width: 200px; display: inline-block;"><p style="font-family: Verdana,Geneva,sans-serif; font-weight: 700; font-size: 20px;">Unidades sanitarias</p></div>
     <div style="width: 150px; display: inline-block;"><img src="{{ public_path('img/logo_mgp_sin_fondo.png') }}"></div>
     <hr style="height: 10px; background-color: black; width: 100%;">
@@ -17,7 +19,7 @@
     <p style="font-family: Verdana,Geneva,sans-serif;">Rp/</p>
     <div style="background-image: url('{{ public_path('img/back_receta.png')}}');background-repeat: no-repeat; background-position: center; min-height: 75% !important;">
             @foreach($recetas as $receta)
-            <p style="font-family: Verdana,Geneva,sans-serif; text-align: left; font-size: 14px; line-height:1px;">{{ $receta }}</p>
+            <p style="font-family: Verdana,Geneva,sans-serif; text-align: left; font-size: 14px; line-height:2px;">{{ $receta }}</p>
         @endforeach
     
         <p style="font-family: Verdana,Geneva,sans-serif; text-align: left; font-size: 14px;"><i>Diagnóstico: Proceso hormonal de modificación</i></p>
